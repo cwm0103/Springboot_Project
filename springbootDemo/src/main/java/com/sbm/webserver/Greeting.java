f3bdde74fd554a7d77f51bf2a96c0f2238ce9583
chenwangming <371396574@qq.com>
2018-01-04 16:10:39 +0800

    添加 Building a RESTful Web Service

---
+++ springbootDemo/src/main/java/com/sbm/webserver/Greeting.java
@@ -0,0 +1,22 @@
+package com.sbm.webserver;
+
+/**
+ * Created by chenwangming on 2018/1/4.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
