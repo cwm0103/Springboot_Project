33cfcb096f9b428b207c6a77aa109cf4d6305490
chenwangming <sa>
2018-01-17 17:06:57 +0800

    添加行程任务计划  ScheduledTask

---
+++ springbootDemo/src/main/java/com/sbm/config/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package com.sbm.config;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+    private static final org.slf4j.Logger log= LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private  static final SimpleDateFormat format=new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void CurrentDateTime()
+    {
+        log.info("The time is now {}",format.format(new Date()));
+    }
+
+}
