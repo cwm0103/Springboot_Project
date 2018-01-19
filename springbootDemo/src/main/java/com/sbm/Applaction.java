64abd2a76506a9e23106b315ed22cbad8eea2a42
chenwangming <sa>
2018-01-19 13:38:37 +0800

    提交有调整的地方

--- springbootDemo/src/main/java/com/sbm/Applaction.java
+++ springbootDemo/src/main/java/com/sbm/Applaction.java
@@ -2,6 +2,7 @@ package com.sbm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
