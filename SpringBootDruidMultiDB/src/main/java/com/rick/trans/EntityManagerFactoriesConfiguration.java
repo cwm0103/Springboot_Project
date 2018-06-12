8e873b890ddaea84743f73a80c753c4b49fb5f3d
chenwangming <sa>
2018-06-12 10:50:55 +0800

    //2018-6-12 添加JPA事务

---
+++ SpringBootDruidMultiDB/src/main/java/com/rick/trans/EntityManagerFactoriesConfiguration.java
@@ -0,0 +1,25 @@
+package com.rick.trans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+@Configuration
+public class EntityManagerFactoriesConfiguration {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean emf() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan(
+                new String[]{"com.rick.entities"});
+        emf.setJpaVendorAdapter(
+                new HibernateJpaVendorAdapter());
+        return emf;
+    }
+}
