1b6a659c26ea7824d166ca310067f4b5a7ef2bba
cwm0103 <371396574@qq.com>
2018-08-10 14:54:39 +0800

    cwm add 2018-8-10 添加登陆方法

--- bjly-manage/bjly-web/web-api/src/main/java/com/bjly/webapi/apicontroller/UserApiController.java
+++ bjly-manage/bjly-web/web-api/src/main/java/com/bjly/webapi/apicontroller/UserApiController.java
@@ -1,16 +1,19 @@
 package com.bjly.webapi.apicontroller;
 
+import com.bjly.webapi.utils.AjaxResult;
 import com.bjly.webentity.userManage.LyUser;
 import com.bjly.webservice.financeManage.LyTestService;
 import com.bjly.webservice.userManage.LyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
+@CrossOrigin
 public class UserApiController {
 
     @Autowired
@@ -27,18 +30,51 @@ public class UserApiController {
     @ResponseBody
     public Object addUser()
     {
-        LyUser lyUser=new LyUser();
-        lyUser.setUAge(10);
-        lyUser.setUCode("cwm");
-        lyUser.setUEducation(1);
-        lyUser.setUName("陈王明");
-        lyUser.setUPhoto("//");
-        lyUser.setUSex("1");
-        lyUser.setUPwd("123456");
-        lyUser.setUTel("18500328373");
-        int insert = lyUserService.insert(lyUser,"db4");
+        try
+        {
+            LyUser lyUser=new LyUser();
+            lyUser.setUAge(10);
+            lyUser.setUCode("cwm");
+            lyUser.setUEducation(1);
+            lyUser.setUName("陈王明");
+            lyUser.setUPhoto("//");
+            lyUser.setUSex("1");
+            lyUser.setUPwd("123456");
+            lyUser.setUTel("18500328373");
+            int insert = lyUserService.insert(lyUser,"db4");
+            if(insert>0)
+            {
+                return AjaxResult.success();
+            }
+        }catch (Exception e)
+        {
+            return AjaxResult.error(e.getMessage());
+        }
+        return AjaxResult.success();
+    }
 
-        return insert;
+    /**
+     * 登陆
+     * @param userName
+     * @param password
+     * @return
+     */
+    @RequestMapping("login")
+    @ResponseBody
+    public Object login(String userName,String password)
+    {
+        try{
+            LyUser lyUser = lyUserService.selectLogin(userName, password,"db4");
+            if(lyUser!=null)
+            {
+                return AjaxResult.success(lyUser);
+            }
+            return AjaxResult.error("500");
+        }catch (Exception ex)
+        {
+            return  AjaxResult.error(ex.getMessage());
+        }
+
     }
 
 }
